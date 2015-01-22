30a09e4688703e46531e0177bd25868e06c20c29
Janmejay Rai <janmejay.rai@rsystems.com>
2015-01-22 20:47:20 +0530

    Changes for fetching connections

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -10,24 +10,4 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 			redirect_to new_user_registration_url
 		end
 	end
-
-	def fetch_profiles
-		client = LinkedIn::Client.new(your_consumer_key, your_consumer_secret)
-		request_token = client.request_token({})
-		rtoken = request_token.token
-		rsecret = request_token.secret
-		request_token.authorize_url
-		pin = 12694
-		client.authorize_from_request(rtoken, rsecret, pin)
-		linkedin_requests = client.authorize_from_request(rtoken, rsecret, pin)
-		linkedin_requests = ["aad07521-4220-4083-bda2-0182924969cf", "288e8fe8-ec2a-4cf8-b1a9-b1d497aaf81f"]
-		client.profile
-		client.connections
-		client.connections.keys
-		client.connections.values
-		client.connections.values[3]
-		my_connections = client.connections.values[3]
-		my_connections.last.values
-		my_connections.first.values
-	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,27 @@ class User < ActiveRecord::Base
 			end
 		end
 	end
+
+	def self.fetch_connections
+		consumer_key = "78dtq60tch3i8c"
+		consumer_secret = "h6ku1JYIU5KA0UpR"
+		pin = 12694
+		client = LinkedIn::Client.new(consumer_key, consumer_secret)
+		request_token = client.request_token({})
+		rtoken = request_token.token
+		rsecret = request_token.secret
+		request_token.authorize_url
+		pin = 12694
+		client.authorize_from_request(rtoken, rsecret, pin)
+		linkedin_requests = client.authorize_from_request(rtoken, rsecret, pin)
+		linkedin_requests = ["aad07521-4220-4083-bda2-0182924969cf", "288e8fe8-ec2a-4cf8-b1a9-b1d497aaf81f"]
+		client.profile
+		client.connections
+		client.connections.keys
+		client.connections.values
+		client.connections.values[3]
+		my_connections = client.connections.values[3]
+		# my_connections.last.values
+		# my_connections.first.values
+	end
 end
