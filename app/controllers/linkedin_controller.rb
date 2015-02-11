9a24097b6a5e8484d8b4025eec42104108257956
Janmejay Rai <janmejay.rai31@gmail.com>
2015-02-10 16:38:02 -0800

    added more fields for fetching users information

--- app/controllers/linkedin_controller.rb
+++ app/controllers/linkedin_controller.rb
@@ -1,5 +1,6 @@
 # http://sreeharikmarar.blogspot.in/2013/08/linkedin-gem-with-rails-4-how-to-import.html
 # https://developer.linkedin.com/documents/profile-fields
+# http://zapone.org/blaine/2015/01/04/rails-linkedin-gem-not-able-to-fetch-connectionss-email-ids/
 
 class LinkedinController < ApplicationController
 
@@ -8,7 +9,7 @@ class LinkedinController < ApplicationController
 		secret = Constants::LINKEDIN_APP_KEY
 		linkedin_configuration = { :site => 'https://api.linkedin.com',
 				:authorize_path => '/uas/oauth/authenticate',
-				:request_token_path =>'/uas/oauth/requestToken?scope=r_basicprofile+r_fullprofile+r_emailaddress+r_network+r_contactinfo',
+				:request_token_path =>'/uas/oauth/requestToken?scope=r_basicprofile+r_fullprofile+r_emailaddress+r_network+r_contactinfo+rw_groups+rw_nus',
 				:access_token_path => '/uas/oauth/accessToken' }
 		@linkedin_client = LinkedIn::Client.new(key, secret,linkedin_configuration )
 	end
@@ -33,10 +34,30 @@ class LinkedinController < ApplicationController
 		end
 
 		c = @linkedin_client
-		
-		profile_1 = c.profile(:fields=>["first_name","last_name","headline","public_profile_url","date-of-birth","main_address","phone-numbers","primary-twitter-account","twitter-accounts","location"])
+
+		# fetching basic profile
+		# profile_1 = c.profile(:fields=>["first_name","last_name","headline","public_profile_url","date-of-birth","main_address","phone-numbers","primary-twitter-account","twitter-accounts","location"])
+		profile_1 = c.profile(:fields=>["id","first-name","last-name","maiden-name","formatted-name","phonetic-first-name","phonetic-last-name","formatted-phonetic-name","headline","industry","distance","current-share","num-connections","num-connections-capped","summary","specialties","positions","picture-url","site-standard-profile-request","public-profile-url"])
 
 		puts "profile_1 = #{profile_1}"
+
+		#fetching email address.
+		profile_email_address = c.profile(:fields=>["email-address"])
+
+		#fetching full profile.
+		full_profile = c.profile(:fields=>["last-modified-timestamp","proposal-comments","associations","interests","publications","patents","languages","skills","certifications","educations","courses","volunteer","three-current-positions","three-past-positions","num-recommenders","recommendations-received","following","job-bookmarks","suggestions","date-of-birth","member-url-resources","related-profile-views","honors-awards"])
+
+		#fetching contact info
+		contact_info = c.profile(:fields=>["phone-numbers","bound-account-types","im-accounts","main-address","twitter-accounts","primary-twitter-account"])
+
+		#fetching connections
+		connections = c.connections
+
+		#fetching group memberships
+		grp_memberships = c.profile(:fields=>["group-memberships"])
+
+		#fetching network details
+		ntw_details = c.profile(:fields=>["network"])
 
 		profile_2 = c.profile(:fields=>["positions","three_current_positions","three_past_positions","publications","patents"])
 
